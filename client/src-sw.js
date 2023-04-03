7bbd2226061522358b16b41db71aaf19e86027a4
Daria <dariabatiuk5@gmail.com>
2023-04-02 22:48:31 -0300

    fixed src-sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,6 @@
 const { warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+// const {  } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
